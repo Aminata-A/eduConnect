5acff935a4a9964921722dbb795c180a3ad9d028
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-05-20 19:30:29 +0000

    Finition des fonctionnalites

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
- var copy = document.querySelector('.logo-slide').cloneNode(true);
- document.querySelector(".logos").appendChild(copy)
+//  var copy = document.querySelector('.logo-slide').cloneNode(true);
+//  document.querySelector(".logos").appendChild(copy)
+
+document.getElementById('burger').addEventListener('click', () => {
+    menu = document.getElementById('menu')
+    currentDisplay = menu.style.display
+    if(currentDisplay == "" || currentDisplay == "none"){
+        menu.style.display = 'flex'
+    } else {
+        menu.style.display = 'none'
+    }
+})
